247679ee4c0d17ffd1bdaaaca4e64ffb81f62bdd
BrianF-2021 <88780659+BrianF-2021@users.noreply.github.com>
2023-09-24 22:31:20 -0400

    Update mysqlconnection.py

--- my_app/config/mysqlconnection.py
+++ my_app/config/mysqlconnection.py
@@ -6,7 +6,7 @@ class MySQLConnection:
         # change the user and password as needed
         connection = pymysql.connect(host = 'localhost',
                                     user = 'root',
-                                    password = '1Thes5:18',
+                                    password = os.environ['SQLPW'],
                                     db = db,
                                     charset = 'utf8mb4',
                                     cursorclass = pymysql.cursors.DictCursor,
